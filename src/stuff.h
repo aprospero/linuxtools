42981e154fa1f5dba4e5bc253c1044a9ebb98929
aprospero <apro@posteo.de>
2023-06-10 18:22:37 +0200

    Standard stuff that would be missed if not there.

---
+++ src/stuff.h
@@ -0,0 +1,23 @@
+#ifndef _H_LINUXTOOLS_STUFF
+#define _H_LINUXTOOLS_STUFF
+
+// standard stuff that would be missed if not there
+
+#ifndef NULL
+#  define NULL ((void*) 0)
+#endif
+
+#ifndef FALSE
+#  define FALSE  0
+#endif
+
+#ifndef TRUE
+#  define TRUE   (!0)
+#endif
+
+#ifndef ARRLEN
+#define ARRLEN(ARR) (sizeof(ARR) / sizeof((ARR)[0]))
+#endif
+
+
+#endif  // _H_LINUXTOOLS_MISSING
