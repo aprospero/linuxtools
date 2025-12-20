66f2206d27052490090a38a57d83aaa897b09994
Aprospero <apro@posteo.de>
2025-12-20 03:36:10 +0100

    stringhelp: added strincmp

--- src/stringhelp.c
+++ src/stringhelp.c
@@ -1,15 +1,21 @@
-#include <string.h>
 #include <ctype.h>
+#include <stdint.h>
+#include "stringhelp.h"
 
 int stricmp(const char * str1, const char * str2)
 {
+  return strincmp(str1, str2, SIZE_MAX);
+}
+
+int strincmp(const char * str1, const char * str2, size_t len)
+{
   int idx;
-  while (*str1 != '\0' && *str2 != '\0')
+  for (;*str1 != '\0' && *str2 != '\0' && len > 1; ++str1, ++str2, --len)
   {
     idx = toupper(*str1) - toupper(*str2);
     if (idx)
       return idx;
-    str1++; str2++;
   }
   return toupper(*str1) - toupper(*str2);
 }
+
--- src/stringhelp.h
+++ src/stringhelp.h
@@ -1,12 +1,15 @@
 #ifndef _H_LINUXTOOLS_STRINGHELP
 #define _H_LINUXTOOLS_STRINGHELP
 
+#include <stddef.h>
+
 #ifdef __cplusplus
 extern "C"
 {
 #endif
 
   int stricmp(const char * str1, const char * str2);
+  int strincmp(const char * str1, const char * str2, size_t len);
 
 #ifdef __cplusplus
 }
